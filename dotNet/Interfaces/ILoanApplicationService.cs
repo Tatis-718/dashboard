fd23df5c3e8bb4b54060ae46b4d0fd4e11533b4b
Mendenzon Tatis Rosario <128002783+Tatis-718@users.noreply.github.com>
2023-07-03 08:28:20 -0500

    dotNet folder

---
+++ dotNet/Interfaces/ILoanApplicationService.cs
@@ -0,0 +1,18 @@
+﻿using Sabio.Models;
+using Sabio.Models.Domain.LoanApplications;
+using Sabio.Models.Requests.LoanApplications;
+using System.Collections.Generic;
+
+namespace Sabio.Services.Interfaces
+{
+    public interface ILoanApplicationService
+    {
+        int Insert(LoanApplicationAddRequest model, int UserId);
+        void Delete(int id);
+        void Update(LoanApplicationUpdateRequest model, int userId);
+        Paged<LoanApplicationBorrowerBusiness> GetAllPaginated(int pageIndex, int pageSize);
+        Paged<LoanApplicationBorrowerBusiness> GetByTypePaginated(int loanTypeId, int pageIndex, int pageSize);
+        List<LoanApplication> GetCurrentPage(int pageIndex, int pageSize, int userId);
+
+    }
+}
---
+++ dotNet/Models/BorrowerDashboard.cs
@@ -0,0 +1,16 @@
+﻿using Sabio.Models.Domain.Blogs;
+using Sabio.Models.Domain.Lenders;
+using Sabio.Models.Domain.LoanApplications;
+using Sabio.Models.Files;
+using System.Collections.Generic;
+
+namespace Sabio.Models.Domain.DashBoards
+{
+    public class BorrowerDashboard
+    {
+        public Paged<Blog> Blogs { get; set; }
+        public List<LoanApplication> LoanApplications { get; set; }
+        public Paged<Lender> Lenders { get; set; }
+        public Paged<File> Files { get; set; }
+    }
+}
